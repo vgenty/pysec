5464bdc5d7baa8ef9df218c86b9e58ea08bf34e7
Victor Genty <vgenty@nevis.columbia.edu>
2015-09-22 14:41:35 -0400

    ftp tested downloads to TMP, unzips, then will send data to xbrl.py

--- test_ftp.py
+++ test_ftp.py
@@ -1,4 +1,5 @@
 import pysec.edgar_config as ec
+import os
 
 from pysec.util import ftp_connector
 
@@ -13,5 +14,14 @@ y = None
 for i in b:
     if ec.FTP_REGEX.search(i) is not None:
         y = i; break;
+        
+filename = '0001104659-09-053270-xbrl.zip'
+
+f = open('/tmp/%s' % filename,'wb')
+ftp._connection.retrbinary("RETR " + filename, f.write)
+f.close()
+
+os.chdir('/tmp/')
+k = os.system('unzip -o %s' % filename)
 
 print y
